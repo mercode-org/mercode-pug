916671fc553c8f8cd32d8553ef52807b3658bf8a
yutyo <tanakinci2002@gmail.com>
2019-11-09 20:03:37 +0300

    Added donation pop-up and fixed several bugs.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,16 @@
 window.$ = require('jquery')
 
-$(document).ready(function () {
-  $('.menu-toggle').click(function () {
+$(document).ready(function() {
+  $('.menu-toggle').click(function() {
     $('nav').toggleClass('active')
   })
 })
+
+
+function copyClipboard() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Copied MyEther Wallet Adress:" + copyText.value);
+}

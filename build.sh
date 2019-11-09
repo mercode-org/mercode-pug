9e64ecaab7bb827215f319821435a7e168e0146c
yutyo <tanakinci2002@gmail.com>
2019-11-10 01:31:15 +0300

    Edited build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ git pull origin master
 pug -w ./ -o ./mercode.org
 pug -w ./tr -o ./mercode.org/tr
 cp -fpr ./mercode.org /var/www
+rm -r /var/www/hub.mercode.org/portal.html 
+cp -s /var/www/mercode.org/portal.html /var/www/hub.mercode.org/portal.html
 echo Done!

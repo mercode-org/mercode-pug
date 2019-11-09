f7b99b85d3fa9fccdd00877f4eec72615d154ab6
yutyo <tanakinci2002@gmail.com>
2019-11-10 00:47:28 +0300

    Bug fix

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ echo Copyleft 2019 Mercode - Protected by GPLv3 certificate.
 git pull origin master
 pug -w ./ -o ./public
 pug -w ./tr -o ./public/tr
-cp -fpr ./public /var/www/mercode.org
+cp -fpr ./public /var/www/mercode.org/
 echo Done!

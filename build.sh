4d43a36d4c14c70e1639dbbfdf0c2614119aa3ba
yutyo <tanakinci2002@gmail.com>
2020-01-18 20:21:15 +0300

    clear the code and code improve

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ echo Copyleft 2019 Mercode - Protected by GPLv3 certificate.
 echo Stashing local changes...
 git reset --hard origin/master
 git pull -f origin master
+npm install --package-lock-only --no-package-lock pug
 pug -w ./ -o ./mercode.org
 pug -w ./tr -o ./mercode.org/tr
 cp -fpr ./mercode.org /var/www
--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 echo Copyleft 2019 Mercode - Protected by GPLv3 certificate.
+npm install --package-lock-only --no-package-lock pug
 pug -w ./ -o ./mercode.org
 pug -w ./tr -o ./mercode.org/tr
 echo Done!

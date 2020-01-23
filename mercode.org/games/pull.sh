903585169dde0fff304d1cf601b9f623a901006a
yutyo <tanakinci2002@gmail.com>
2020-01-23 13:57:26 +0300

    Added games.

---
+++ mercode.org/games/pull.sh
@@ -0,0 +1,8 @@
+echo Copyleft 2019-2020 Mercode - Protected by GPLv3 certificate.
+echo Deleting repos...
+rm -r dwarfs2019
+rm -r distrocards
+echo Cloning repos...
+git clone https://github.com/mercode-org/dwarfs2019.git
+git clone https://github.com/mercode-org/distrocards
+echo Done!

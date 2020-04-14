98f3ccfbd10e9e580e38b83a5d5c85ae8feb1254
yutyo <tanakinci2002@gmail.com>
2020-04-14 21:16:35 +0300

    switch to nginx

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ pug -w ./tr -o ./mercode.org/tr
 cp -fpr ./mercode.org /var/www
 rm -r /var/www/hub.mercode.org/portal.html
 cp -s /var/www/mercode.org/portal.html /var/www/hub.mercode.org/portal.html
-echo Purging Apache2...
-service apache2 reload
-service apache2 restart
-service apache2 reload
+echo Purging Nginx...
+sudo systemctl reload nginx
+sudo systemctl restart nginx
+sudo systemctl reload nginx
 echo Done!

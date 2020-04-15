240411c26ea6229b899de4d05fc41d209db0769b
yutyo <tanakinci2002@gmail.com>
2020-04-16 01:04:19 +0300

    add new game and add game pull config to package.json

--- mercode.org/games/pull.sh
+++ mercode.org/games/pull.sh
@@ -1,8 +1,10 @@
 echo Copyleft 2019-2020 Mercode - Protected by GPLv3 certificate.
 echo Deleting repos...
-rm -r dwarfs2019
-rm -r distrocards
+sudo rm -r mercode.org/games/dwarfs2019
+sudo rm -r mercode.org/games/distrocards
+sudo rm -r mercode.org/games/flappux
 echo Cloning repos...
-git clone https://github.com/mercode-org/dwarfs2019.git
-git clone https://github.com/mercode-org/distrocards
+git clone https://github.com/mercode-org/dwarfs2019.git mercode.org/games/dwarfs2019
+git clone https://github.com/mercode-org/distrocards mercode.org/games/distrocards
+git clone https://github.com/ardacebi/flappux mercode.org/games/flappux
 echo Done!
